cacbb35157638b25bdf71d6c5c93a048806173b5
Diego Scott <99587513+2003scott@users.noreply.github.com>
2023-08-13 00:50:42 -0500

    ejercico4-5-6

---
+++ Ejercicio4-5-6/src/main/java/com/ejercicio456/Ejercicio456/Ejercicio456Application.java
@@ -0,0 +1,13 @@
+package com.ejercicio456.Ejercicio456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio456Application.class, args);
+	}
+
+}
